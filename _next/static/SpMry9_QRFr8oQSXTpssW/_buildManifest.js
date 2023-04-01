76d3913f01abbdbe0b975eae2a4fc9bb61ca186d
amcoscabaresh <32603332+amcoscabaresh@users.noreply.github.com>
2023-04-01 19:28:07 +0000

    Deploying to public from @ amcoscabaresh/TeacherSalaries@af5b16a8118800c37b1dcecd4a2454337ffd9699 🚀

---
+++ _next/static/SpMry9_QRFr8oQSXTpssW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/98-e057e9c15c41b227.js","static/chunks/pages/index-209099033d0fd3dc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
